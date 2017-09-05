289564cb8dc7ca22f93381c3d9a42af21a2b9008
anwarshk17 <anwar17111992@gmail.com>
2017-09-05 22:37:55 +0530

    Added the event listener on the button to open the modal and created a function to open the modal.

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ const button = document.getElementById('modalBtn');     // Get the modal button
 const modal = document.getElementById('simpleModal');   // Get the modal element
 const closeBtn = document.getElementById('modalCloseBtn');   // Get the close button
 
+button.addEventListener('click', openModal);    // Added the event listener for opening the modal.
+
+// function for opening the modal
+function openModal() {
+    console.log('Modal Opened');
+    modal.style.display = 'block';
+}
