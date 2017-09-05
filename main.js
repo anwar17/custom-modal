1e5d2f46ad92aa4cbf5e252401cdd9aa77c079bf
anwarshk17 <anwar17111992@gmail.com>
2017-09-05 22:32:14 +0530

    Created the link for all the main components in the javascript file.

---
+++ main.js
@@ -0,0 +1,4 @@
+const button = document.getElementById('modalBtn');     // Get the modal button
+const modal = document.getElementById('simpleModal');   // Get the modal element
+const closeBtn = document.getElementById('modalCloseBtn');   // Get the close button
+
